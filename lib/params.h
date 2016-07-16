19fa8d0b074b20745f154c5ef55d92e3029f98e6
tennismichel <tennismichel@web.de>
2016-07-16 07:05:49 +0200

    ...

--- lib/params.h
+++ lib/params.h
@@ -3,24 +3,25 @@
 
 /*-----------------------------------------------------------------------*/
   //Global constants
-  #define SAMPLE_RATE 3     //update Frequency for sensors and Rotors
+  #define SAMPLE_RATE 5     //update Frequency for sensors and Rotors
 
 /*-----------------------------------------------------------------------*/
   //PID constants
   #define MAX_E_FOR_I 45    //Maximum angle to contribute to I_Controller
   #define MIN_E_FOR_I 0.3   //Minimum angle to start I_controller
-  #define K_P_START   0.00042         //Global gain on PIDControl
-  #define T_I_START   2400         //T_N
-  #define T_D_START   500        //T_V
+  #define K_P_START   0.0002         //Global gain on PIDControl
+  #define T_I_START   10000         //T_N
+  #define T_D_START   700        //T_V
 
   /*--------------------------------------*/
   /*PID control setups
 
-    Setup         K_P       T_I       T_D       SampleRate  TakeOff   PWMOUT1     PWMOUT2
+    Setup         K_P       T_I       T_D       SampleRate  TakeOff  Rotor    PWMOUT1     PWMOUT2
 
-    Frame M2      0.012     0         5000      5           1150      5           3
-    Testbed M2    0.00042   2400      500       5           1150      3           5
-    Testbed M2    0.00018   2400      600       3           1450      3           5
+    Frame M2      0.012     0         5000      5           1150     Green    5           3
+    Testbed M2    0.00042   2400      500       5           1150     Green    3           5
+    Testbed M2    0.000165  10000     710       3           1450     Green    3           5
+    Testbed M2    0.000200  10000     820       5           1450     Green    3           5
 
   */
   /*--------------------------------------*/
