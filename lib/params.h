b7d5effcf06928d3c6752e9785ced88528ed8506
chrismolli <molliere.c@gmail.com>
2016-09-29 11:54:25 +0200

    Added params.h

--- lib/params.h
+++ lib/params.h
@@ -10,10 +10,10 @@
   #define MAX_E_FOR_I 45    //Maximum angle to contribute to I_Controller
   #define MIN_E_FOR_I 0.3   //Minimum angle to start I_controller
 
-  #define K_P_START   0.009         //Global gain on PIDControl
-  #define T_I_START   400         //T_N
-  #define T_D_START   60          //T_V
-  #define T_DD_START  17          //only used in PIDD2Control.h
+  #define K_P_START   0.0085         //Global gain on PIDControl
+  #define T_I_START   400           //T_N
+  #define T_D_START   155           //T_V
+  #define T_DD_START  125            //only used in PIDD2Control.h
   /*--------------------------------------*/
   /*PID control setups
 
@@ -26,25 +26,25 @@
     Testbed M2    0.0005    2400      400           5           1150     Red      3           5
     Testbed M2    0.00045   2400      550           5           1300     Red      3           5
     Testbed M2    0.0004    2400      680           5           1300     Red      3           5
-    Testbed M2    0.006     400       100   20      5           1250     Red      3           5
-    Testbed M2    0.0085    400       70    15      5/3         1250/1350 Red      3           5
-    -->für SR = 200HZ ist so 0.007/8 das Maximum für Kp. Gutes Verhältnis für T_D/T_DD = 5
-    -->Verhältnis optimieren und eventuell mit Matlab simulieren versuchen
+    Testbed M2    0.001     2000      330   370     5           1250     Red      3           5
+    Frame M2      0.007     400       121   103     5           1200     Red      6           5
+    Frame M2      0.0085    400       100   86      5           1200     Red      6           5
+    Frame M4      0.0085    400       155   125     5           1200     Red
   */
   /*--------------------------------------*/
 
 /*-----------------------------------------------------------------------*/
   //Rotorcontrol constants
   //define Output Pins for ESCs
-  #define ESCPIN1 3 //für Chris hier ne 6
+  #define ESCPIN1 6 //für Chris hier ne 6
   #define ESCPIN2 5
   #define ESCPIN3 6
   #define ESCPIN4 9
 
   //RotorSignal limits (from 1012 until 2012 you have increase in F)
-  #define MAX_ROTOR_SIGNAL 1600
+  #define MAX_ROTOR_SIGNAL 1500
   #define MIN_ROTOR_SIGNAL 1015
-  #define TAKE_OFF_SIGNAL  1250
+  #define TAKE_OFF_SIGNAL  1200
 
 /*-----------------------------------------------------------------------*/
   //Sensor constants
@@ -52,10 +52,5 @@
   #define GYRORANGE 2
   #define MAG_PLUGGED_IN FALSE
   #define DECLINATION_ANGLE_DEGREE 2.1664
-
-  /*-----------------------------------------------------------------------*/
-    //NMEAProcessing constants
-    #define RX_PIN 1
-    #define TX_PIN 2
 
 #endif
