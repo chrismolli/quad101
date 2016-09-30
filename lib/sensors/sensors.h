044bc2121e4076da5c6e274cd7776c8132142bc4
chrismolli <molliere.c@gmail.com>
2016-09-30 11:56:52 +0200

    Added bmp180 to sensors lib

--- lib/sensors/sensors.h
+++ lib/sensors/sensors.h
@@ -7,20 +7,46 @@
 /*==================================================================*/
   //Sensor libraries
   #include "IMU/imu.h"
+  #include "BMP180/bmp.h"
+  #include "../params.h"
 
 /*==================================================================*/
   //Class definition
   class SENSORS{
     public:
-      IMU imu;
-      
+      IMU imu;  //includes compass
+      BMP180 bmp;
       void begin(void);
+      void getActive(void);
+      void update(void);
+
+    private:
+      uint8_t number_of_active_sensors;
   };
 
 /*==================================================================*/
   //Functions
   void SENSORS::begin(void){
     imu.begin();
+    if(BMP_PLUGGED_IN) bmp.begin();
   }
+
+  void SENSORS::update(void){
+    imu.update(SAMPLE_RATE);
+    bmp.update();
+  }
+
+  void SENSORS::getActive(void){
+    number_of_active_sensors = 1;
+    if(MAG_PLUGGED_IN) number_of_active_sensors++;
+    if(BMP_PLUGGED_IN) number_of_active_sensors++;
+
+    if(Serial){
+        Serial.print(number_of_active_sensors);
+        Serial.println(" active sensors found!");
+    }
+  }
+
+
 
 #endif
