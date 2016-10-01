2e0ff25eaf0a152308b73637fab8331ed98e5b72
chrismolli <molliere.c@gmail.com>
2016-10-01 13:58:09 +0200

    Implemented usr into sensors lib

--- lib/sensors/sensors.h
+++ lib/sensors/sensors.h
@@ -8,6 +8,7 @@
   //Sensor libraries
   #include "IMU/imu.h"
   #include "BMP180/bmp.h"
+  #include "USR/usr.h"
   #include "../params.h"
 
 /*==================================================================*/
@@ -16,9 +17,11 @@
     public:
       IMU imu;  //includes compass
       BMP180 bmp;
+      USR usr;
       void begin(void);
       void getActive(void);
       void update(void);
+      void updateSlow(void);
 
     private:
       uint8_t number_of_active_sensors;
@@ -29,17 +32,23 @@
   void SENSORS::begin(void){
     imu.begin();
     if(BMP_PLUGGED_IN) bmp.begin();
+    if(USR_PLUGGED_IN) usr.begin();
   }
 
   void SENSORS::update(void){
     imu.update(SAMPLE_RATE);
-    bmp.update();
+    if(BMP_PLUGGED_IN) bmp.update();
+  }
+
+  void SENSORS::updateSlow(void){
+    if(USR_PLUGGED_IN) usr.pulseOut();
   }
 
   void SENSORS::getActive(void){
     number_of_active_sensors = 1;
     if(MAG_PLUGGED_IN) number_of_active_sensors++;
     if(BMP_PLUGGED_IN) number_of_active_sensors++;
+    if(USR_PLUGGED_IN) number_of_active_sensors++;
 
     if(Serial){
         Serial.print(number_of_active_sensors);
